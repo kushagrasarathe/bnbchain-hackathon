94f88b054f735a61f345ae1d88cd4f409bcdf7ed
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2024-02-10 15:16:39 +0530

    feat: new next-app

--- frontend/src/app/(dashboard)/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardHome from "@/components/dashboard/dashboard-home";
-
-export default function DashboardPage() {
-  return (
-    <div className="flex items-center justify-center md:py-6 ">
-      <DashboardHome />
-    </div>
-  );
-}
