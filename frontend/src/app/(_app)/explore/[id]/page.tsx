0b46796c4d85d03e63d68e2912741d539a702795
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2024-02-22 00:56:57 +0530

    feat: Added the Explore function code

--- frontend/src/app/(_app)/explore/[id]/page.tsx
+++ frontend/src/app/(_app)/explore/[id]/page.tsx
@@ -1,6 +1,84 @@
-import React from "react";
+"use react";
 
-export default function ResearchPage() {
+import {
+  DAOMember_ABI,
+  DAOMember_Contract_Address,
+} from "@/constants/constants";
+import React, { useEffect, useState } from "react";
+import { erc20Abi, parseEther } from "viem";
+import { useAccount, usePublicClient, useWalletClient } from "wagmi";
+
+export default function ResearchPage({ params }: { params: { id: string } }) {
+  const { address: account, isConnected } = useAccount();
+  const publicClient = usePublicClient();
+  const { data: walletClient } = useWalletClient();
+  const [research, setResearch] = useState<{
+    Id: bigint;
+    Title: any;
+    Description: any;
+    Content: any;
+    Publisher: `0x${string}`;
+    DoP: string;
+  }>();
+
+  // fetched the member.json
+  const fetchIPFS = async (metadataURI: string) => {
+    try {
+      console.log("Fetching from IPFS ...");
+      const URL = `https://ipfs.io/ipfs/${metadataURI}/research.json`;
+      const response = await fetch(URL);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // fetch the data from the contract and then fetched the same from IPFS
+  // returns an object that is then stored in the Array of request
+  const fetchRequests = async (_id: bigint) => {
+    try {
+      console.log(_id);
+      const request = await publicClient?.readContract({
+        account,
+        address: DAOMember_Contract_Address,
+        abi: DAOMember_ABI,
+        functionName: "getResearch",
+        args: [_id],
+      });
+      if (!request) {
+        console.log("No request found");
+        return;
+      }
+      console.log(request);
+      console.log(request.researchPaperURI);
+      const _date = new Date(
+        Number(request.dateOfPublication)
+      ).toLocaleString();
+      console.log(_date);
+      const response = await fetchIPFS(request.researchPaperURI);
+      console.log(response);
+      const parsedRequest = {
+        Id: _id,
+        Title: response.Name,
+        Description: response.Description,
+        Content: response.Content,
+        Publisher: request.researcher,
+        DoP: _date,
+      };
+      console.log(parsedRequest);
+      setResearch(parsedRequest);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (params.id && !research) {
+      fetchRequests(BigInt(params.id));
+    }
+  }, [params.id]);
+
   return (
     <div>
       <h1 className=" text-4xl font-semibold">Research Title Here</h1>
--- frontend/src/app/(_app)/explore/page.tsx
+++ frontend/src/app/(_app)/explore/page.tsx
@@ -1,10 +1,105 @@
-import React from "react";
+"use client";
 import sample from "@/assets/sample.jpeg";
 import Image from "next/image";
 import ResearchCard from "./research-card";
 import Link from "next/link";
 
+import {
+  DAOMember_ABI,
+  DAOMember_Contract_Address,
+} from "@/constants/constants";
+import React, { useEffect, useState } from "react";
+import { erc20Abi, parseEther } from "viem";
+import { useAccount, usePublicClient, useWalletClient } from "wagmi";
+
 export default function ExplorePage() {
+  const { address: account, isConnected } = useAccount();
+  const publicClient = usePublicClient();
+  const { data: walletClient } = useWalletClient();
+  const [researches, setResearches] =
+    useState<
+      ({ Id: bigint; Title: any; Description: any; Content: any } | undefined)[]
+    >();
+
+  // fetched the member.json
+  const fetchIPFS = async (metadataURI: string) => {
+    try {
+      console.log("Fetching from IPFS ...");
+      const URL = `https://ipfs.io/ipfs/${metadataURI}/research.json`;
+      const response = await fetch(URL);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // fetch the data from the contract and then fetched the same from IPFS
+  // returns an object that is then stored in the Array of request
+  const fetchRequests = async (_id: bigint) => {
+    try {
+      const request = await publicClient?.readContract({
+        account,
+        address: DAOMember_Contract_Address,
+        abi: DAOMember_ABI,
+        functionName: "getResearch",
+        args: [_id],
+      });
+      if (!request) {
+        console.log("No request found");
+        return;
+      }
+      console.log(request.researchPaperURI);
+      const response = await fetchIPFS(request.researchPaperURI);
+      const parsedRequest = {
+        Id: _id,
+        Title: response.Name,
+        Description: response.Description,
+        Content: response.contentURL,
+      };
+      console.log(parsedRequest);
+      return parsedRequest;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // fetches the no. of requests , then fetches the each request and store the result in the array of requests
+  const get = async () => {
+    try {
+      // setLoading(true);
+      // setMessage("Fetching data...");
+      console.log("starting ...");
+      const data = await publicClient?.readContract({
+        account,
+        address: DAOMember_Contract_Address,
+        abi: DAOMember_ABI,
+        functionName: "counterResearches",
+      });
+
+      const total = Number(data);
+      const promises = [];
+      console.log(total);
+      for (let id = 0; id < total; id++) {
+        const requestsPromise = fetchRequests(BigInt(id));
+        promises.push(requestsPromise);
+      }
+      const _researches = await Promise.all(promises);
+      console.log(_researches);
+      console.log("ending...");
+      /// set the array of the objects of the requests is stored and can be rendered then
+      setResearches(_researches);
+      // setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    get();
+  }, []);
+  // fetch the researches
+
   return (
     <div className="space-y-5 py-6">
       <div className="space-y-1">
