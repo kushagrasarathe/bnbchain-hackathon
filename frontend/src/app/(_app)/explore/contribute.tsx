2618210413699e81c81247ee8f4adda4b2a51abd
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2024-03-02 17:02:40 +0530

    feat: contribute Modal function added with mint

--- frontend/src/app/(_app)/explore/contribute.tsx
+++ frontend/src/app/(_app)/explore/contribute.tsx
@@ -14,24 +14,137 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-
+import { erc20Abi, parseEther } from "viem";
+import { useAccount, usePublicClient, useWalletClient } from "wagmi";
 interface ContributeForm {
   amount: number;
 }
-
-const initialFormValues: ContributeForm = {
-  amount: 0,
-};
+import {
+  ContributorNFT_ABI,
+  ContributorNFT_Contract_Address,
+  DAOFunds_Contract_Address,
+  DAOMember_ABI,
+  DAOMember_Contract_Address,
+} from "@/constants/constants";
 
 export default function Contribute() {
-  const [formValues, setFormValues] =
-    useState<ContributeForm>(initialFormValues);
+  const { address: account, isConnected } = useAccount();
+  const publicClient = usePublicClient();
+  const { data: walletClient } = useWalletClient();
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
+  const [amount, setAmount] = useState<number>();
+  const [tokenAddress, setTokenAddress] = useState<`0x${string}`>(
+    "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
+  );
+
+  const contribute = async () => {
+    try {
+      if (!publicClient) {
+        // setIsLoading(false);
+        console.log("No Wallet Detected");
+        return;
+      }
+      if (!walletClient) {
+        // setIsLoading(false);
+        console.log("No Wallet Detected");
+        return;
+      }
+      if (!amount || !tokenAddress) {
+        console.log("Inputs missing ");
+        return;
+      }
+
+      if (
+        tokenAddress &&
+        tokenAddress !== "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
+      ) {
+        const data = await publicClient.simulateContract({
+          account,
+          address: tokenAddress,
+          abi: erc20Abi,
+          functionName: "transfer",
+          args: [DAOFunds_Contract_Address, parseEther(amount.toString())],
+        });
+
+        const tx = await walletClient.writeContract(data.request);
+        console.log("Transaction Sent");
+        const transaction = await publicClient.waitForTransactionReceipt({
+          hash: tx,
+        });
+        console.log(transaction);
+        console.log(data.result);
+        // setIsLoading(false);
+        return {
+          transaction,
+          data,
+        };
+      } else if (
+        tokenAddress === "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
+      ) {
+        const tx = await walletClient.sendTransaction({
+          account,
+          to: DAOFunds_Contract_Address,
+          value: parseEther(amount.toString()),
+        });
+
+        console.log("Transaction Sent");
+        const transaction = await publicClient.waitForTransactionReceipt({
+          hash: tx,
+        });
+
+        console.log(transaction);
+        // setIsLoading(false);
+        return {
+          transaction,
+        };
+      } else {
+        console.log("Invalid Token Address");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const mintContributorNFT = async () => {
+    try {
+      if (!account) {
+        console.log("No Account Detected ,Connect Wallet !!");
+        return;
+      }
+      if (!publicClient) {
+        // setIsLoading(false);
+        console.log("No Wallet Detected");
+        return;
+      }
+      const data = await publicClient.simulateContract({
+        account,
+        address: ContributorNFT_Contract_Address,
+        abi: ContributorNFT_ABI,
+        functionName: "safeMint",
+        args: [account],
+      });
+
+      if (!walletClient) {
+        // setIsLoading(false);
+        console.log("No Wallet Detected");
+        return;
+      }
+
+      const tx = await walletClient.writeContract(data.request);
+      console.log("Transaction Sent");
+      const transaction = await publicClient.waitForTransactionReceipt({
+        hash: tx,
+      });
+      console.log(transaction);
+      console.log(data.result);
+      // setIsLoading(false);
+      return {
+        transaction,
+        data,
+      };
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -50,14 +163,16 @@ export default function Contribute() {
               <Input
                 type="number"
                 name="amount"
-                value={formValues.amount}
-                onChange={handleChange}
+                value={amount}
+                onChange={(e) => setAmount(Number(e.target.value))}
                 placeholder="1000"
                 className="bg-[#f4f3f0] rounded-none"
               />
             </Label>
           </div>
-          <Button className="w-full rounded-none">Contribute</Button>
+          <Button onClick={contribute} className="w-full rounded-none">
+            Contribute
+          </Button>
         </div>
       </DialogContent>
     </Dialog>
